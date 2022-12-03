4f80f2e060950528e9ecfc133f1a4376aee26ff7
menezesphill <phillipe.o.m@hotmail.com>
2022-12-03 17:40:36 -0300

    feat: web3 modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import HomeComponent from "../components/Home";
 import Navbar from "../components/Navbar";
+import Web3ModalProvider from "../contexts/web3modal";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <HomeComponent />
+      <Web3ModalProvider>
+        <Navbar />
+        <HomeComponent />
+      </Web3ModalProvider>
     </>
   );
 }
