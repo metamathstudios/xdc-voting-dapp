835ded42bebd33fdfd612a4af44d96323c15b36c
Joao Vinicius <viniciuspkfr@gmail.com>
2022-12-13 00:02:56 -0300

    feat: add new tag component

--- components/ProposalEditor/index.tsx
+++ components/ProposalEditor/index.tsx
@@ -1,9 +1,8 @@
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
 import styles from './styles.module.scss'
 
-import { TagsInput } from 'react-tag-input-component'
 import '@uiw/react-md-editor/markdown-editor.css'
 import '@uiw/react-markdown-preview/markdown.css'
 import Slider from '@mui/material/Slider'
@@ -12,108 +11,183 @@ import LinkInput from './components/LinkInput'
 import ImageInput from './components/ImageInput'
 import ImageInputRemovable from './components/ImageInputRemovable'
 
-const MDEditor = dynamic(
-  () => import('@uiw/react-md-editor'),
-  { ssr: false }
-)
+const MDEditor = dynamic(() => import('@uiw/react-md-editor'), { ssr: false })
 
 import back from './assets/back.svg'
 import preview from './assets/preview.svg'
 import publish from './assets/publish.svg'
+import less from './assets/less.svg'
+import more from './assets/more.svg'
 
 const ProposalEditorComponent = () => {
-  const [selected, setSelected] = useState(['XDC'])
-
-  const [value, setValue] = useState('# XDC Proposal Editor');
-
+  const [value, setValue] = useState('# XDC Proposal Editor')
   const [renderNumberLink, setRenderNumberLink] = useState(0)
   const [renderNumberImage, setRenderNumberImage] = useState(0)
+  const [dropdown, setDropdown] = useState(true)
+  const [treasury, setTreasury] = useState(false)
+  const [core, setCore] = useState(true)
+  const [xdc, setXdc] = useState(false)
+  const [urgent, setUrgent] = useState(false)
 
   const handleRenderNumberLink = () => {
-    setRenderNumberLink(current => current + 1)
+    setRenderNumberLink((current) => current + 1)
   }
 
   const handleRemoveNumberLink = () => {
-    setRenderNumberLink(current => current - 1)
+    setRenderNumberLink((current) => current - 1)
   }
 
   const handleRenderNumberImage = () => {
-    setRenderNumberImage(current => current + 1)
+    setRenderNumberImage((current) => current + 1)
   }
 
   const handleRemoveNumberImage = () => {
-    setRenderNumberImage(current => current - 1)
+    setRenderNumberImage((current) => current - 1)
   }
 
   const valuetext = (value: number) => {
     return `${value}`
   }
 
+  useEffect(() => {
+    if ((core && treasury) && (xdc && urgent)) {
+      setDropdown(false)
+    } else {
+      setDropdown(true)
+    }
+  }, [core, treasury, xdc, urgent])
+
   return (
     <div className={styles.container}>
       <div className={styles.centerColumn}>
         <div className={styles.userOptions}>
-          <div className={styles.backContainer} onClick={() => window.open('/', '_self')}>
-            <Image src={back} alt='Back' width={17}/>
-            <div className={styles.text} >
-              Back
-            </div>
+          <div
+            className={styles.backContainer}
+            onClick={() => window.open('/', '_self')}
+          >
+            <Image src={back} alt='Back' width={17} />
+            <div className={styles.text}>Back</div>
           </div>
-          
+
           <div className={styles.rightContainer}>
             <div className={styles.preview}>
               <div className={styles.image}>
                 <Image src={preview} alt='Preview' />
               </div>
 
-              <div className={styles.text}>
-                Preview
-              </div>
+              <div className={styles.text}>Preview</div>
             </div>
 
             <div className={styles.publish}>
               <div className={styles.image}>
-                <Image src={publish} alt='Publish' />
+                <Image src={publish} alt='Publish' width={16} />
               </div>
 
-              <div className={styles.text}>
-                Publish
-              </div>
+              <div className={styles.text}>Publish</div>
             </div>
           </div>
         </div>
 
         <div className={styles.inputsContainer}>
           <div className={styles.textInput}>
-            <div className={styles.label}>
-              Title
-            </div>
+            <div className={styles.label}>Title</div>
 
             <div className={styles.input}>
-              <input type="text" />
+              <input type='text' />
             </div>
           </div>
 
           <div className={styles.tagInput}>
-            <div className={styles.label}>
-              TAG (Optional)
-            </div>
+            <div className={styles.label}>TAG (Optional)</div>
 
             <div className={styles.input}>
-              <TagsInput
-                value={selected}
-                onChange={setSelected}
-                name='tags'
-                placeHolder='Enter tags'
-              />
+              {treasury == true ? (
+                <div className={styles.tagContainer} onClick={() => setTreasury(!treasury)}>
+                  <div className={styles.text}>Treasury</div>
+
+                  <div className={styles.image}>
+                    <Image src={less} alt='Remove' />
+                  </div>
+                </div>
+              ) : null}
+
+              {core == true ? (
+                <div className={styles.tagContainer} onClick={() => setCore(!core)}>
+                  <div className={styles.text}>Core</div>
+
+                  <div className={styles.image}>
+                    <Image src={less} alt='Remove' />
+                  </div>
+                </div>
+              ) : null}
+
+              {xdc == true ? (
+                <div className={styles.tagContainer} onClick={() => setXdc(!xdc)}>
+                  <div className={styles.text}>XDC Community</div>
+
+                  <div className={styles.image}>
+                    <Image src={less} alt='Remove' />
+                  </div>
+                </div>
+              ) : null}
+
+              {urgent == true ? (
+                <div className={styles.tagContainer} onClick={() => setUrgent(!urgent)}>
+                  <div className={styles.text}>Urgent</div>
+
+                  <div className={styles.image}>
+                    <Image src={less} alt='Remove' />
+                  </div>
+                </div>
+              ) : null}
             </div>
+
+            {dropdown == true ? <div className={styles.dropdown}>
+              {treasury == false ?
+                <div className={styles.tagContainer} onClick={() => setTreasury(true)}>
+                  <div className={styles.text}>Treasury</div>
+
+                  <div className={styles.image}>
+                    <Image src={more} alt='Add' />
+                  </div>
+                </div>
+              : null}
+
+              {core == false ?
+                <div className={styles.tagContainer} onClick={() => setCore(true)}>
+                  <div className={styles.text}>Core</div>
+
+                  <div className={styles.image}>
+                    <Image src={more} alt='Add' />
+                  </div>
+                </div>
+              : null}
+
+              {xdc == false ?
+                <div className={styles.tagContainer} onClick={() => setXdc(true)}>
+                  <div className={styles.text}>XDC Community</div>
+
+                  <div className={styles.image}>
+                    <Image src={more} alt='Add' />
+                  </div>
+                </div>
+              : null}
+
+              {urgent == false ?
+                <div className={styles.tagContainer} onClick={() => setUrgent(true)}>
+                  <div className={styles.text}>Urgent</div>
+
+                  <div className={styles.image}>
+                    <Image src={more} alt='Add' />
+                  </div>
+                </div>
+              : null}
+            </div> : null}
           </div>
 
           <div className={styles.inputRow}>
             <div className={styles.startDate}>
-              <div className={styles.label}>
-                Start Date
-              </div>
+              <div className={styles.label}>Start Date</div>
 
               <div className={styles.input}>
                 <input type='date' />
@@ -121,9 +195,7 @@ const ProposalEditorComponent = () => {
             </div>
 
             <div className={styles.endDate}>
-              <div className={styles.label}>
-                End Date
-              </div>
+              <div className={styles.label}>End Date</div>
 
               <div className={styles.input}>
                 <input type='date' />
@@ -133,9 +205,7 @@ const ProposalEditorComponent = () => {
 
           <div className={styles.inputRow}>
             <div className={styles.votingToll}>
-              <div className={styles.label}>
-                Voting Toll
-              </div>
+              <div className={styles.label}>Voting Toll</div>
 
               <div className={styles.input}>
                 <input type='number' placeholder='Min: 0 XDC Max: 100000 XDC' />
@@ -143,9 +213,7 @@ const ProposalEditorComponent = () => {
             </div>
 
             <div className={styles.sliderInput}>
-              <div className={styles.label}>
-                Minimum Pass Vote
-              </div>
+              <div className={styles.label}>Minimum Pass Vote</div>
 
               <div className={styles.input}>
                 <Slider
@@ -163,21 +231,33 @@ const ProposalEditorComponent = () => {
           </div>
 
           <div className={styles.textEditor}>
-            <div className={styles.label}>
-              Description
-            </div>
+            <div className={styles.label}>Description</div>
 
-            <div className={styles.input} data-color-mode='light' >
-              <MDEditor value={value} onChange={(text) => setValue(text || '')} height={350} />
+            <div className={styles.input} data-color-mode='light'>
+              <MDEditor
+                value={value}
+                onChange={(text) => setValue(text || '')}
+                height={350}
+              />
             </div>
           </div>
 
           <div className={styles.bottomInputs}>
             <LinkInput handleRenderNumber={handleRenderNumberLink} />
-            { Array.from({length: renderNumberLink}).map((_, i) => <LinkInputRemovable key={i} handleRenderNumber={handleRemoveNumberLink} />)}
+            {Array.from({ length: renderNumberLink }).map((_, i) => (
+              <LinkInputRemovable
+                key={i}
+                handleRenderNumber={handleRemoveNumberLink}
+              />
+            ))}
 
-            <ImageInput handleRenderNumber={handleRenderNumberImage}/>
-            { Array.from({length: renderNumberImage}).map((_, i) => <ImageInputRemovable key={i} handleRenderNumber={handleRemoveNumberImage} />)}
+            <ImageInput handleRenderNumber={handleRenderNumberImage} />
+            {Array.from({ length: renderNumberImage }).map((_, i) => (
+              <ImageInputRemovable
+                key={i}
+                handleRenderNumber={handleRemoveNumberImage}
+              />
+            ))}
           </div>
         </div>
       </div>
