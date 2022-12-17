77f30690de32ed39d49cd5f1478e8ec6f464f8f9
menezesphill <phillipe.o.m@hotmail.com>
2022-12-17 17:48:34 -0300

    fix: dropdown visibility

--- components/Home/components/Search/index.tsx
+++ components/Home/components/Search/index.tsx
@@ -33,9 +33,9 @@ const Search = () => {
             <input type="text" placeholder="Search" />
           </div>
         </div>
-        {!account && 
+        {account &&
           <div
-            style={text == 'XDC Community' ? {width: "260px", margin: '0px 0px 0px -10px' } : {width: "240px", margin: '0px 0px 0px -10px' }}
+            style={text == 'XDC Community' ? { width: "260px", margin: '0px 0px 0px -10px' } : { width: "240px", margin: '0px 0px 0px -10px' }}
             onClick={() => {
               route.push("/editor");
             }}
@@ -43,56 +43,55 @@ const Search = () => {
             <Button icon={newicon} text="New Proposal" />
           </div>
         }
-
-        {!account && 
-          <div className={styles.dropdownContainer} onClick={() => setDropdown(!dropdown)}>
-            <div className={styles.label}>
+        
+        <div className={styles.dropdownContainer} onClick={() => setDropdown(!dropdown)}>
+          <div className={styles.label}>
             <span>{text}</span>
-            </div>
+          </div>
 
-            <div className={styles.icon}>
-              <Image src={dropdownsvg} alt='Drop' />
-            </div>
+          <div className={styles.icon}>
+            <Image src={dropdownsvg} alt='Drop' />
+          </div>
 
-            {dropdown &&
-              <div className={styles.dropdown}>
-                <div className={styles.optionsColumn}>
-                  <div className={styles.option} onClick={() => setText('All')}>
-                    <span>All</span>
-                  </div>
+          {dropdown &&
+            <div className={styles.dropdown}>
+              <div className={styles.optionsColumn}>
+                <div className={styles.option} onClick={() => setText('All')}>
+                  <span>All</span>
+                </div>
 
-                  <div className={styles.option} onClick={() => setText('Active')}>
-                    <span>Active</span>
-                  </div>
+                <div className={styles.option} onClick={() => setText('Active')}>
+                  <span>Active</span>
+                </div>
 
-                  <div className={styles.option} onClick={() => setText('Pending')}>
-                    <span>Pending</span>
-                  </div>
+                <div className={styles.option} onClick={() => setText('Pending')}>
+                  <span>Pending</span>
+                </div>
 
-                  <div className={styles.option} onClick={() => setText('Closed')}>
-                    <span>Closed</span>
-                  </div>
+                <div className={styles.option} onClick={() => setText('Closed')}>
+                  <span>Closed</span>
+                </div>
 
-                  <div className={styles.option} onClick={() => setText('Core')}>
-                    <span>Core</span>
-                  </div>
+                <div className={styles.option} onClick={() => setText('Core')}>
+                  <span>Core</span>
+                </div>
 
-                  <div className={styles.option} onClick={() => setText('Treasury')}>
-                    <span>Treasury</span>
-                  </div>
+                <div className={styles.option} onClick={() => setText('Treasury')}>
+                  <span>Treasury</span>
+                </div>
 
-                  <div className={styles.option} onClick={() => setText('XDC Community')}>
+                <div className={styles.option} onClick={() => setText('XDC Community')}>
                   <span>XDC Community</span>
-                  </div>
+                </div>
 
-                  <div className={styles.option} onClick={() => setText('Urgent')}>
-                    <span>Urgent</span>
-                  </div>
+                <div className={styles.option} onClick={() => setText('Urgent')}>
+                  <span>Urgent</span>
                 </div>
               </div>
-            }
-          </div>
-        }
+            </div>
+          }
+        </div>
+
       </div>
     </>
   );
